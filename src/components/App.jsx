faa2dc91349e785910a1d004c8316c067665cc8b
Maharshi Choksi <Maharshichokshi2003@gmail.com>
2023-03-13 13:46:48 -0400

    changed error route dest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from '../Routes/Home'
+import ErrPage from './ErrPage'
 
 function App() {
 
 	return (
 		<div className="App flex flex-col min-h-screen">
 			<BrowserRouter>
-				<Home />
+				<Routes>
+					<Route path="/" element={<Home />}/>
+					<Route path="*" element={<ErrPage />}/>
+				</Routes>
 			</BrowserRouter>
 		</div>
 	)
