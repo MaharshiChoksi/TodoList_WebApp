a1486879b4152ba15dbabdd4b9ea90980b48a404
Maharshi Choksi <Maharshichokshi2003@gmail.com>
2023-03-31 11:49:26 -0400

    Rearranging the files and adding the contact form

--- src/Routes/ErrPage.jsx
+++ src/Routes/ErrPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import img from '../assets/404.jpg'
 import { Link } from 'react-router-dom'
 
-export default function ErrPage() {
+const ErrPage = () => {
     return (<div className="flex flex-grow w-full h-full items-center justify-center" >
         <div className="flex flex-col gap-5 items-center w-full h-fit m-5">
             <div className='font-bold font-mono text-center lg:text-5xl text-2xl'>Oops! Page Not Found</div>
@@ -12,3 +12,5 @@ export default function ErrPage() {
     </div>
     )
 }
+
+export default ErrPage;
