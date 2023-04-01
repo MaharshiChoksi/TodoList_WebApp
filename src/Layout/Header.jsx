d9e50ae9825907116d453f15aa9ca32682fa4f7e
Maharshi Choksi <Maharshichokshi2003@gmail.com>
2023-04-01 18:43:53 -0400

    Added the email service

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 const Menu = ({ ChangeState }) => {
     return (
         <div className='absolute flex min-w-full min-h-screen z-10 backdrop-blur-sm items-center justify-center top-0'>
-            <div className='flex flex-col gap-5 p-[10%] items-center relative border-2 border-gray-400 rounded-xl font-mono max-sm:text-3xl text-4xl md:text-6xl backdrop-blur-sm bg-violet-900/30'>
+            <div className='flex flex-col gap-5 p-[10%] items-center relative border-2 border-gray-400 rounded-xl font-mono max-sm:text-3xl text-4xl md:text-6xl bg-neutral-900/95'>
                 <button onClick={() => ChangeState(false)}><img src={close} className='absolute top-2 right-2 h-10 max-md:h-9' alt='Close Menu' /></button>
                 <Link to="/" onClick={() => ChangeState(false)} className='hover:border-b-2 hover:text-blue-500 border-solid border-blue-500 duration-75 rounded-sm'>Home</Link>
                 <Link to="/contact" onClick={() => ChangeState(false)} className='hover:border-b-2 hover:text-blue-500 border-solid border-blue-500 duration-75 rounded-sm'>Help</Link>
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,31 +1,61 @@
 import { useForm } from "react-hook-form";
 import { Footer } from "../Layout/Footer";
 import { Header } from "../Layout/Header";
+import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser'
 
 const HelpForm = () => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const NoError = (FormData) => {
+        // get the response of the mail and if success then show success message and reset form or else show error message
+        emailjs.send(
+            // .env
+            'service_r4tbp7f',
+            'template_ke6tefm',
+            FormData,
+            'ZUqNt19M8ssKcfSOa'
+        ).then((result) => {
+            toast.success("Response Sent Successfully", { position: toast.POSITION.TOP_RIGHT });
+            console.log(result);
+            reset();
+        }).catch((err) => {
+            toast.error("Server Error! Please try again letter", { position: toast.POSITION.TOP_RIGHT });
+            console.log(err);
+        });
+    }
+
     // create toaster showing the status of the email send or not (if success green/ red)
-    const SubmitStatus = () => {
-        reset(values => ({
-            ...values,
-            Name: "",
-            Email: "",
-            Message: ""
-        }))
+    const SubmitStatus = (data, event) => {
+        // preventing default submission behaviour
+        event.preventDefault();
+        // Checking errors
+        (errors.name || errors.Email || data.Message.trim() == '') ?
+            // If error found in the form
+            toast.warning("Please Enter Valid values in the form!", { position: toast.POSITION.TOP_RIGHT }) : NoError(data);;
     }
 
     return (
-        <div className="flex flex-grow min-w-full justify-center items-start p-10 bg-black font-mono font-bold">
+        <div className="flex flex-col flex-grow min-w-full items-center p-10 bg-black font-mono font-bold">
+            <ToastContainer autoClose={3000} />
             <form onSubmit={handleSubmit(SubmitStatus)} className='grid grid-cols-half gap-5 p-5 items-start h-fit justify-center border-2 border-gray-400 rounded-xl font-mono bg-violet-900/30'>
                 <div className="col-span-2 text-center pb-7">Contact Form</div>
                 <label>Name </label>
-                <input type="text" placeholder="Name" {...register("Name", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" maxLength={25} size={27} />
+                <input type="text" name="Name" placeholder="Name" {...register("Name", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" maxLength={25} size={27} />
                 <label>Email </label>
-                <input type="email" placeholder="Email" {...register("Email", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" size={27} />
+                <input type="email" name="Email" placeholder="Email" {...register("Email", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" size={27} />
                 <label>Message </label>
-                <textarea placeholder="Your message goes Here..." {...register("Message", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" size={27} rows={5} />
+                <textarea name="Message" placeholder="Your message goes Here..." {...register("Message", { required: true, })} className="rounded-2xl pl-2 font-medium text-black" size={27} rows={5} />
                 <input type="submit" className="socialmedia col-span-2 bg-lime-600 text-black font-bold" />
             </form>
+            <div className='max-md:grid grid-cols-2 grid-row-2 max-md:rounded-3xl inline-flex gap-5 text-center mt-12 p-5 border-2 border-white borer-solid rounded-full'>
+                <Link to="https://www.linkedin.com/in/maharshi-choksi-155143208/" target='_blank' className='socialmedia'>LinkedIn</Link>
+                <Link to="https://github.com/GDeveloper22/" target='_blank' className='socialmedia'>Github</Link>
+                <Link to="https://discordapp.com/users/694116811712626728" target='_blank' className='socialmedia'>Discord</Link>
+                <Link to="https://www.instagram.com/maharshi_2003/" target='_blank' className='socialmedia'>Instagram</Link>
+            </div>
         </div>
     )
 }
