8e815aacdab5de3df494f974d54e4b71d9deb06c
Maharshi Choksi <Maharshichokshi2003@gmail.com>
2023-04-14 14:04:35 -0400

    Increased the height of the edit area

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,10 +6,10 @@ const EditBar = ({ props, ChgData, setShowHiddenDiv }) => {
     const data = useRef();
 
     return (<div className='fixed flex min-w-full min-h-[100%] z-10 backdrop-blur-sm items-center justify-center top-0'>
-        <div className='flex flex-col gap-5 m-[10%] p-[5%] items-center relative border-2 border-gray-400 rounded-xl font-mono max-sm:text-xl text-4xl max-md:text-3xl bg-neutral-900/95'>
+        <div className='flex flex-col gap-5 m-[10%] p-[3%] items-center relative border-2 border-gray-400 rounded-xl font-mono max-sm:text-xl text-4xl max-md:text-3xl bg-neutral-900/95'>
             <button onClick={() => setShowHiddenDiv(false)}><img src={close} className='absolute top-2 right-2 h-10 max-md:h-9' alt='Close Menu' /></button>
             <div>Edit your Task: </div>
-            <textarea type="text" name="task" defaultValue={props.data} placeholder="Edit your task" className=" rounded-xl bg-black border-2 text-white p-2 text-xl max-sm:w-30 max-lg:h-30 max-lg:text-lg" ref={data} />
+            <textarea type="text" name="task" defaultValue={props.data} placeholder="Edit your task" className=" rounded-xl bg-black border-2 text-white p-2 h-96 text-xl max-sm:w-30 max-lg:h-72 max-lg:text-sm" ref={data} />
             <input type="submit" className='socialmedia' onClick={() => (ChgData(props.id, data.current.value), setShowHiddenDiv(false))} />
         </div>
     </div>)
@@ -22,7 +22,7 @@ export const Task = ({ props, onDelete, toggleStatus, ChgData }) => {
     const borderColor = props.completed ? "border-green-500" : "border-yellow-400";
 
     return (<>        <div className={`task flex flex-col items-center p-4 mt-5 gap-5 max-lg:gap-5 max-md:text-sm max-w-3xl border-2 ${borderColor} rounded-xl text-center`}>
-        <span className={`${props.completed && 'line-through'} text-start`} onClick={() => setShowHiddenDiv(!showHiddenDiv)}>{props.data}</span>
+        <pre className={`${props.completed && 'line-through'} text-start`} onClick={() => setShowHiddenDiv(!showHiddenDiv)}>{props.data}</pre>
         <div className="flex max-xs:flex-col items-center min-w-fit justify-evenly gap-5 w-full">
             <input type="checkbox" className="scale-150" checked={props.completed} onClick={(e) => { e.target.checked ? toggleStatus(props.id, true, DateTime()) : toggleStatus(props.id, false, DateTime()) }} />
             <button className="min-w-fit h-fit border-2 rounded-lg text-lg max-lg:text-sm" onClick={() => onDelete(props.id)}>Delete</button>
